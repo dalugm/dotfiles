fd07ed4b14818a4bc3144e5b58421baa9048af01
gangzhan <moutong945@163.com>
2018-10-08 23:02:05 +0800

    hyperline doesn't support hyper2 yet

--- hyper.js
+++ hyper.js
@@ -152,9 +152,38 @@ module.exports = {
 
         hyperline: {
             plugins: [
-                "ip",
-                "cpu",
-                "spotify"
+                {
+                    name: 'hostname',
+                    options: { color: 'lightBlue' }
+                },
+                {
+                    name: 'memory',
+                    options: { color: 'white' }
+                },
+                {
+                    name: 'uptime',
+                    options: { color: 'lightYellow' }
+                },
+                {
+                    name: 'cpu',
+                    options: {
+                        colors: {
+                            high: 'lightRed',
+                            moderate: 'lightYellow',
+                            low: 'lightGreen'
+                        }
+                    }
+                },
+                {
+                    name: 'network',
+                    options: { color: 'lightCyan' }
+                },
+                {
+                    name: 'battery',
+                    options: {
+                        colors: { fine: 'lightGreen', critical: 'lightRed' }
+                    }
+                }
             ]
         },
 
