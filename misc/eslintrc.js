1240ea07de3f813222fc5e87ef47e0b5fe6a5213
dalu <moutong945@outlook.com>
2022-04-15 08:31:24 +0800

    daily update

---
+++ misc/eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  "env": {
+    "browser": true
+  },
+  "extends": [
+    "eslint:recommended",
+  ],
+  "parserOptions": {
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "plugins": [
+  ],
+  "rules": {
+    quotes: ["error", "double"],
+    semi: ["error", "always"]
+  },
+  "globals": {
+  }
+}
