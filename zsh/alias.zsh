d4dbddee4f9fe730910a6069debec9662ca00894
dalu <mou.tong@outlook.com>
2023-04-19 12:11:05 +0800

    Cleanup alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -1,14 +1,8 @@
 #!/usr/env/bin bash
 
-# Set personal aliases
+# Set personal aliases.
 # For a full list of active aliases, run `alias`.
 #
-
-alias bashconf="vim ~/.bashrc"
-alias zshconf="vim ~/.zshrc"
-alias tmuxconf="vim ~/.tmux.conf"
-alias alaconf="vim ~/.alacritty.yml"
-alias ytdlconf="vim ~/.config/youtube-dl/config"
 
 alias g='git'
 alias px='proxychains4'
@@ -17,18 +11,19 @@ alias tem='TERM=xterm-24bit emacs'
 alias vi='vim'
 alias nvi='nvim'
 
-# proxy
+# Proxy.
 export hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
-alias all_proxy="export http_proxy=http://127.0.0.1:1080;export https_proxy=http://127.0.0.1:1087;export all_proxy=socks5://127.0.0.1:1080"
+alias all_proxy="export http_proxy=http://127.0.0.1:1080;export https_proxy=http://127.0.0.1:1080;export all_proxy=socks5://127.0.0.1:1080"
 alias wsl_all_proxy="export http_proxy=http://${hostip}:10810;export https_proxy=http://${hostip}:10810;export all_proxy=socks5://${hostip}:10810"
-alias clean_proxy="export all_proxy=; export http_proxy=; export https_proxy="
+alias clean_proxy="export http_proxy=;export https_proxy=;export all_proxy="
 
-# TERM
+# TERM.
 alias x256_term="export TERM=xterm-256color"
 alias x24_term="export TERM=xterm-24bit"
 alias s256_term="export TERM=screen-256color"
 alias s24_term="export TERM=screen-24bit"
-# List directory contents
+
+# List directory contents.
 alias l="ls -ahlG"
 
 # https://stackoverflow.com/questions/3746/whats-in-your-bashrc
