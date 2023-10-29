91646a09629c2e062b87b2c2554c6df32eef6745
dalu <mou.tong@outlook.com>
2023-10-29 18:00:03 +0800

    Update powershell config

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,17 +1,2 @@
 # basic
 Set-PSReadLineOption -EditMode Emacs
-
-# For zoxide v0.8.0+
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell | Out-String)
-})
-
-# For older versions of zoxide
-Invoke-Expression (& {
-    $hook = if ($PSVersionTable.PSVersion.Major -lt 6) { 'prompt' } else { 'pwd' }
-    (zoxide init --hook $hook powershell) -join "`n"
-})
-
-# fnm
-fnm env --use-on-cd | Out-String | Invoke-Expression
