43c90f9106d9fb28fe6e6e52a78228e063e173a3
dalu <mou.tong@outlook.com>
2024-08-24 17:06:34 +0800

    Add nix-darwin

---
+++ nix-darwin/flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "nix configuration for macOS";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    nix-darwin = {
+      url = "github:LnL7/nix-darwin";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ self, nixpkgs, nix-darwin }:
+  let
+    configuration = { pkgs, ... }: {
+      environment.systemPackages =
+        [ pkgs.vim
+        ];
+
+      # Auto upgrade nix package and the daemon service.
+      services.nix-daemon.enable = true;
+      # nix.package = pkgs.nix;
+
+      # Necessary for using flakes on this system.
+      nix.settings.experimental-features = "nix-command flakes";
+
+      # Create /etc/zshrc that loads the nix-darwin environment.
+      programs.zsh.enable = true;  # default shell on catalina
+      # programs.fish.enable = true;
+
+      # Set Git commit hash for darwin-version.
+      system.configurationRevision = self.rev or self.dirtyRev or null;
+
+      # Used for backwards compatibility, please read the changelog before changing.
+      # $ darwin-rebuild changelog
+      system.stateVersion = 4;
+
+      # The platform the configuration will be used on.
+      nixpkgs.hostPlatform = "x86_64-darwin";
+
+      system.defaults = {
+        dock.autohide = true;
+        loginwindow.LoginwindowText = "nix with darwin";
+      };
+
+      homebrew = {
+        enable = true;
+        brews = [];
+        casks = [
+          "neovide"
+        ];
+      };
+    };
+  in
+  {
+    # Build darwin flake using:
+    # $ darwin-rebuild build --flake .#dalum
+    darwinConfigurations."dalum" = nix-darwin.lib.darwinSystem {
+      modules = [ configuration ];
+    };
+
+    # Expose the package set, including overlays, for convenience.
+    darwinPackages = self.darwinConfigurations."dalum".pkgs;
+  };
+}
