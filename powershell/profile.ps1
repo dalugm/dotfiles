08f867a23f7f83d0fb93faa29390a3ca9e4d6918
dalu <mou.tong@outlook.com>
2024-10-01 14:54:33 +0800

    Add alias for pwsh

--- powershell/profile.ps1
+++ powershell/profile.ps1
@@ -1,2 +1,20 @@
-# basic
+# Basic.
 Set-PSReadLineOption -EditMode Emacs
+
+function Get-FileSize {
+  Get-ChildItem | ForEach-Object {
+    $size = $_.Length;
+    $sizeString = switch ($size) {
+      {$_ -ge 1MB} { "{0:N2} MB" -f ($_ / 1MB); break }
+      {$_ -ge 1KB} { "{0:N2} KB" -f ($_ / 1KB); break }
+      default { "$_ Bytes" }
+    }
+
+    [PSCustomObject]@{
+      Name = $_.Name;
+      Size = $sizeString
+    };
+  } | Format-Table -AutoSize
+}
+
+Set-Alias -Name ll -Value Get-FileSize
