5f136f642195d4b514ac9ba14994260df31691cf
dalu <mou.tong@outlook.com>
2026-06-18 15:48:24 +0800

    Add opencode

--- .config/bash/alias.sh
+++ .config/bash/alias.sh
@@ -3,16 +3,15 @@
 alias g='git'
 alias px='proxychains4'
 alias ec='emacsclient -nw --alternate-editor=nano'
-alias tem='TERM=xterm-24bit emacs'
 alias nv='neovide'
 alias nvt='neovide --frame transparent'
 command_exists helix && alias hx='helix'
 
 # Proxy
 export hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
-alias all_proxy="export http_proxy=http://127.0.0.1:1080;export https_proxy=http://127.0.0.1:1080;export all_proxy=socks5://127.0.0.1:1080"
-alias wsl_all_proxy="export http_proxy=http://${hostip}:10810;export https_proxy=http://${hostip}:10810;export all_proxy=socks5://${hostip}:10810"
-alias clean_proxy="export http_proxy=;export https_proxy=;export all_proxy="
+alias all_proxy="export HTTP_PROXY=http://127.0.0.1:10809;export HTTPS_PROXY=http://127.0.0.1:10809;export ALL_PROXY=socks5://127.0.0.1:10800;export NO_PROXY=localhost,127.0.0.1,::1"
+alias wsl_all_proxy="export HTTP_PROXY=http://${hostip}:10810;export HTTPS_PROXY=http://${hostip}:10810;export ALL_PROXY=socks5://${hostip}:10810;export NO_PROXY=localhost,127.0.0.1,::1"
+alias clean_proxy="export HTTP_PROXY=;export HTTPS_PROXY=;export ALL_PROXY="
 
 # TERM
 alias x256_term="export TERM=xterm-256color"
